56a90609c338634446508f6a233075f9acff663b
Virgile Robles <virgile.robles@protonmail.ch>
2018-05-30 22:28:20 +0200

    Improve beep

--- ml/beep.ml
+++ ml/beep.ml
@@ -4,17 +4,30 @@ open Result
 let pi = 3.14159265359
 let beep_device = ref None
 let beep_callback_ref = ref None
+let beep_playing = ref false
+let beep_last = ref false
 let beep_pos = ref 0
 let beep_sampling = ref 0
 let beep_frequency = 440.
 let beep_volume = 6000.
 let beep_callback buffer =
     for i = 0 to Bigarray.Array1.dim buffer - 1 do
-        let t = float_of_int !beep_pos in
-        let s = float_of_int !beep_sampling in
-        let v = beep_frequency *. 2. *. pi *. t /. s in
-        buffer.{i} <- int_of_float @@ beep_volume *. sin v ;
-        beep_pos := !beep_pos + 1
+        if !beep_playing || !beep_last then (
+            let t = float_of_int !beep_pos in
+            let s = float_of_int !beep_sampling in
+            let v = beep_frequency *. 2. *. pi *. t /. s in
+            buffer.{i} <- int_of_float @@ beep_volume *. sin v ;
+            beep_pos := !beep_pos + 1 ;
+            if !beep_last && (buffer.{i} = 0 ||
+                (i > 1 && (buffer.{i} > 0) != (buffer.{i-1} > 0))) then (
+                buffer.{i} <- 0;
+                beep_last := false ;
+            )
+        )
+        else (
+            buffer.{i} <- 0 ;
+            beep_pos := 0
+        )
     done
 
 let beep_init () =
@@ -42,8 +55,10 @@ let beep_init () =
     | Ok h -> h
     in
     beep_sampling := have.as_freq ;
-    beep_device := Some dev
+    beep_device := Some dev ;
+    Sdl.pause_audio_device dev false
+
 
 let beep_play b =
-    let dev = Option.get !beep_device in
-    Sdl.pause_audio_device dev (not b)
+    beep_playing := b ;
+    if not b then beep_last := true
